6d538683dc5112f8018145954b1e504946483098
hungmol <hungmol1@gmail.com>
2016-12-26 09:48:40 +0700

    Upload 3DScanner Graycode + PhaseShift

---
+++ Modules/Images/loadPhaseImage.m
@@ -0,0 +1,7 @@
+function loadPhaseImage()
+    im1 = imread('../../image/phase1.jpg');
+    im2 = imread('../../image/phase2.jpg');
+    im3 = imread('../../image/phase3.jpg');
+    
+    imshow(im3);
+endfunction
